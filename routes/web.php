407c7038ea119db0e9e9220c80578e6e35b9eb7b
Portal User <portal@example.com>
2026-07-28 16:17:27 +0100

    fix: Improve print view to display applicant data correctly

--- routes/web.php
+++ routes/web.php
@@ -196,8 +196,6 @@ Route::prefix('applicant')->name('applicant.')->group(function () {
         Route::get('/application/edit', [ApplicationController::class, 'editApplication'])->name('application.edit');
         Route::put('/application', [ApplicationController::class, 'updateApplication'])->name('application.update');
         Route::get('/application/print', [ApplicationController::class, 'printApplication'])->name('application.print');
-
-        // Debug route (no auth required)
     });
 });
 
